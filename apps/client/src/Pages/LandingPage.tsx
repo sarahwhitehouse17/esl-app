275f09cb87f7c8fb075884a23927e0390c31d887
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2025-11-17 13:07:45 +0100

    Added sign-in form to landing page

--- apps/client/src/Pages/LandingPage.tsx
+++ apps/client/src/Pages/LandingPage.tsx
@@ -1,8 +1,15 @@
+
+
 export default function landingPage() {
   return (
     <div>
       <h1>Welcome to ESL App</h1>
       <p>Please log in to continue.</p>
+      <form>
+        <input type="text" placeholder="username"></input>
+        <input type="text" placeholder="password"></input>
+        <input type="button" value="submit" />
+      </form>
       {/* You can add login logic later */}
     </div>
   );
